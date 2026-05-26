bfe4fc58c9f3ecf9adbf103bf5de3c15fa80d3ec
Lodewyk Roux <lloderoux@protonmail.ch>
2026-05-26 08:35:32 +0200

    Update User.cs

--- LRouxTech.Core.Auth/Models/User.cs
+++ LRouxTech.Core.Auth/Models/User.cs
@@ -1,6 +1,9 @@
 ﻿namespace LRouxTech.Core.Auth.Models;
 
-public class User
+public class User : BaseModel<User>
 {
-    
+    public required string Name { get; set; }
+    public string? Surname { get; set; }
+    public required string UserName { get; set; }
+    public required string Email { get; set; }
 }
