a88874427b95b4887914cbf97d17b8dbac7f25d8
Lodewyk Roux <lloderoux@protonmail.ch>
2026-05-27 06:51:45 +0200

    Updated structure

--- LRouxTech.Core.Auth/Core/Entities/User.cs
+++ LRouxTech.Core.Auth/Core/Entities/User.cs
@@ -1,1 +1,9 @@
-Move all users here
+﻿namespace LRouxTech.Core.Auth.Models;
+
+public class User : BaseModel<User>
+{
+    public required string Name { get; set; }
+    public string? Surname { get; set; }
+    public required string UserName { get; set; }
+    public required string Email { get; set; }
+}
--- LRouxTech.Core.Auth/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LRouxTech.Core.Auth.Models;
-
-public class User : BaseModel<User>
-{
-    public required string Name { get; set; }
-    public string? Surname { get; set; }
-    public required string UserName { get; set; }
-    public required string Email { get; set; }
-}
