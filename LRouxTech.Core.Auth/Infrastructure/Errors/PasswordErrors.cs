57daeca273c5823f64d80590753d617de567e08a
Lodewyk Roux <lloderoux@protonmail.ch>
2026-05-29 08:33:20 +0200

    Update PasswordErrors.cs

--- LRouxTech.Core.Auth/Infrastructure/Errors/PasswordErrors.cs
+++ LRouxTech.Core.Auth/Infrastructure/Errors/PasswordErrors.cs
@@ -6,6 +6,7 @@ public static class PasswordErrors
 {
     public static readonly Error EmptyPassword = new("Password.EmptyPassword", "Password cannot be empty.");
     public static readonly Error EmptyConfirmPassword = new("Password.EmptyConfirmPassword", "Confirm password cannot be empty.");
+    public static readonly Error PasswordsdontMatch = new("Password.PasswordsdontMatch", "Passwords don't match.");
     public static readonly Error TooShort= new("Password.TooShort", "Password lenght should be 8 or more characters.");
     public static readonly Error NoUppercase = new("Password.NoUppercase", "Password needs at least 1 uppercase character.");
     public static readonly Error NoLowercase = new("Password.NoLowercase", "Password needs at least 1 lowercase character.");
